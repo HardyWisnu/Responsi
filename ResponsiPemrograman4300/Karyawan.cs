233712360db75729367be0ed6e3a813c59f5ed76
HardyWisnu <hardywisnu@amikom.students.ac.id>
2022-04-19 16:41:21 +0700

    responsi4300gagal

---
+++ ResponsiPemrograman4300/Karyawan.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4300
+{
+    internal class Karyawan
+    {
+        public String NIK { get; set; }
+        public String Nama { get; set; }
+        public int Gaji { get; set; }
+        public void kayawan()
+        {
+            Console.WriteLine("N0 |NIK\t\t |Nama\t\t |Gaji");
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("1  |{0}\t |{1}\t\t |{2}", NIK, Nama, Gaji);
+            Console.WriteLine("2  |{0}\t |{1}\t\t |{2}", NIK, Nama, Gaji);
+        }
+
+        public void KenaikanGaji(int Gaji)
+        {
+            int Gajibaru;
+
+            Console.WriteLine("Assik Kenaikan Gaji 10%");
+            Gajibaru = Gaji * 110;
+            Gaji = Gajibaru / 100;
+
+        }
+    }
+}
---
+++ ResponsiPemrograman4300/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4300
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Karyawan karyawan = new Karyawan();
+
+
+            karyawan.NIK = "190302123";
+            karyawan.Nama = "Paijo";
+            karyawan.Gaji = 3000000;
+
+            karyawan.NIK = "190302124";
+            karyawan.Nama = "Jono";
+            karyawan.Gaji = 2000000;
+
+
+            karyawan.kayawan();
+            karyawan.KenaikanGaji(Gaji);
+            karyawan.kayawan();
+
+
+        }
+    }
+}
